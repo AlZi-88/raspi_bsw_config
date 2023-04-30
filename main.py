bfe6a30ed073e30b8e0cb922a0a8711dd9c95132
AlZi-88 <alexander.zimmerer1988@gmail.com>
2023-04-30 18:09:01 +0200

    Updates to match setup

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from bsw_config import *
+from bsw_config import bsw_config
 import time
 
 if __name__ == "__main__":
     output_channels_test=[5, 7, 11,13,15,17]
-    bsw = BSW()
+    bsw = bsw_config.BSW()
     for channel in output_channels_test:
         bsw.set_channel(channel)
         time.sleep(2)
         bsw.reset_channel(channel)
         time.sleep(2)
+    for i in range(0,100,10):
+        bsw.change_pwm_duty(i)
+        time.sleep(5)
 
     del bsw
